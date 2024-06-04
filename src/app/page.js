8677cb17046add3d3583142dc9012ce9dc0ea24d
Shreyas Thakare <shreyasthakare5@gmail.com>
2024-06-04 18:06:42 +0530

    [ST] 4/6/24 aboutus section completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,12 @@
-import Comments from "@/components/Comments";
+import AboutUs from "@/components/AboutUs";
 import FAQS from "@/components/FAQS";
-import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import GetStarted from "@/components/GetStarted";
-import HeroSection from "@/components/HeroSection";
-import InfiniteScroll from "@/components/InfiniteScroll";
 import Journey from "@/components/Journey";
-import Navbar from "@/components/Navbar";
-import Section2Home from "@/components/Section2Home";
-import Section3Home from "@/components/Section3Home";
-import Section4Home from "@/components/Section4Home";
 export default function Home() {
   return (
     <div className="flex flex-col gap-40 bg-[#FEF5F3]">
-      <HeroSection />
-      {/* <Section2Home />
-      <Section3Home />
-      <Section4Home />
-      <Features />
-      <Comments />*/}
+      <AboutUs />
       <GetStarted />
       <Journey />
       <FAQS />
