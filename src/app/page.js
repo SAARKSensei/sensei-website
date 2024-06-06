21cda4f7f7abc7f475ac6636df0a9b70097d1046
Shreyas Thakare <shreyasthakare5@gmail.com>
2024-06-06 11:13:28 +0530

    [ST] 6/6/24 parent issues section completed

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,12 @@ import Footer from "@/components/Footer";
 import GetStarted from "@/components/GetStarted";
 import Journey from "@/components/Journey";
 import OurModules from "@/components/OurModules";
+import ParentIssues from "@/components/ParentIssues";
 import Psychologists from "@/components/Psychologists";
 export default function Home() {
   return (
     <div className="flex flex-col gap-40 bg-[#FEF5F3]">
+      <ParentIssues />
       <Psychologists />
       <OurModules />
       <AboutUs />
