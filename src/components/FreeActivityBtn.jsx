3a82bef146d19360a5f3074430bb7a4138d817d1
Shreyas Thakare <shreyasthakare5@gmail.com>
2024-06-21 13:13:35 +0530

    [ST] 21/6/24 links added

--- src/components/FreeActivityBtn.jsx
+++ src/components/FreeActivityBtn.jsx
@@ -1,10 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const FreeActivityBtn = () => {
     return (
-        <div className="max-w-[258px] md:max-w-fit h-12 md:h-14 rounded-[50px] px-6 md:px-8 py-3 md:py-4 bg-grad_1 cursor-pointer">
+        <Link href={"https://webapp.sensei.org.in/"} className="max-w-[258px] md:max-w-fit h-12 md:h-14 rounded-[50px] px-6 md:px-8 py-3 md:py-4 bg-grad_1 cursor-pointer">
             <p className="text-white text-center body1_b">Sign Up for FREE activity</p>
-        </div>
+        </Link>
     )
 }
 
